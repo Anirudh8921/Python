8f10620a1b53626ad8201ae3bc3310756b6fc197
Anirudh8921 <veluruanirudh08@gmail.com>
2024-12-23 18:22:00 +0530

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,6 @@
+Food=['burger','veg pizza','momos','Chinese','garlic bread','french fries','non-veg pizza']
+print('Data Type:',type(Food))
+print('total food items:',len(Food))
+print('food at present not available:',Food[2:5])
+Food.append('kabab')
+print('add one more item:',Food)
