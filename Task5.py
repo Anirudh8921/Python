a6de49b25cb210c9f0fa064308bca90ca7d4fb75
Anirudh8921 <veluruanirudh08@gmail.com>
2024-12-30 18:36:18 +0530

    Update Task5.py

--- Task5.py
+++ Task5.py
@@ -1,34 +1,14 @@
-list=eval(input('enter a number:'))
-# print(list[0])
-if (list[0]<list[1])and(list[0]<list[2]):
-    print('minimum',list[0])
-elif (list[1]<list[2]):
-    print('minimum',list[1])
-else:
-    print('minimum',list[2])
-if (list[0]>list[1])and(list[0]>list[2]):
-    print('maximum',list[0])
-elif (list[1]>list[2]):
-    print('maximum',list[1])
-else:
-    print('maximum',list[2])
-total=0
-for i in list:
-    total=total+i
-print('total',total)
-
-
-# list_1=eval(input('enter a number:'))
-# max=list_1[0]
-# min=list_1[0]
-# tot=0
-# for i in list_1:
-#     if i>max:
-#         max=i
-#     if i<min:
-#         min=i
-#     tot=tot+i
+list_1=eval(input('enter a number:'))
+max=list_1[0]
+min=list_1[0]
+tot=0
+for i in list_1:
+    if i>max:
+        max=i
+    if i<min:
+        min=i
+    tot=tot+i
     
-# print(max)
-# print(min)
-# print(tot)
+print(max)
+print(min)
+print(tot)
